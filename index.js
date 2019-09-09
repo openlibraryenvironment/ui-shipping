e172c1eda63b715c59af09c77ec819e2f257db4b
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-09-09 11:45:46 +0100

    copied index.js from ui-request, shipping now has a borrowed ui

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import ResourceSharing from '@folio/rs';
 
-export default (props) => (
-  <span>
-    <h1>This is the shipping app.</h1>
-    <p>Ship loaned items to borrowing library</p>
-  </span>
-);
+export default (props) => <ResourceSharing {...props} />;
