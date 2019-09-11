55d3c321366560a468bfcd64988e9e8c18daec8d
Ethan Freestone <ethan.freestone@k-int.co.uk>
2019-09-11 16:56:54 +0100

    Added some functionality, created search and sort pane

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Shipments from './components/views/Shipments.js'
+
+
+import { Redirect, Route, Switch } from 'react-router-dom';
+import AppNameContext from '../AppNameContext';
+import css from './index.css';
+
+
+
+class Shipping extends React.Component {
+  static propTypes = {
+    match: PropTypes.object.isRequired,
+    actAs: PropTypes.string.isRequired,
+    stripes: PropTypes.shape({
+      connect: PropTypes.func,
+      logger: PropTypes.shape({
+        log: PropTypes.func,
+      }),
+    }),
+  };
+
+
+   constructor(props) {
+    super(props);
+  }
+
+  onClose() {
+    <p>Closing</p>
+  }
+
+  render() {
+    const {
+      actAs,
+      match: {
+        path
+      }
+    } = this.props;
+
+    const appName = path.substring(1).replace(/\/.*/, '');
+
+    if (actAs === 'settings') {
+      return <Settings {...this.props} appName={appName} />;
+    }
+      return (
+        <AppNameContext.Provider value={appName}>
+          <div className={css.container}>
+            <div className={css.body}>
+              <Switch>
+                <Redirect
+                  exact
+                  from={path}
+                  to={`${path}/shipments`}
+                />
+                <Route path={`${path}/shipments`} component={Shipments} />
+              </Switch>
+            </div>
+          </div>
+        </AppNameContext.Provider>
+    );
+
+  }
+}
+
+export default Shipping;
+export {default as Shipments } from './components/views/Shipments'
+
+//export default (props) => <ResourceSharing {...props} />;
